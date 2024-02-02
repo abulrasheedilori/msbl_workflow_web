03dc86b0f3f95a7ba87a57a5a342ba0079ee0b5b
Abdulrasheed Ilori <aaailori@gmail.com>
2024-02-02 16:30:31 +0100

    env var resolved

--- src/store/apiService.ts
+++ src/store/apiService.ts
@@ -11,7 +11,7 @@ import User, {
 } from "./apiTypes";
 
 const api = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 // api.defaults.headers.common["Access-Control-Allow-Origin"] = origin;
 // api.defaults.headers.common["Access-Control-Allow-Headers"] =
