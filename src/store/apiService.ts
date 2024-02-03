106863f9939004f73588ec5ce0d37ca6d7c6892a
Abdulrasheed Ilori <aaailori@gmail.com>
2024-02-03 13:27:13 +0100

    resolving error504 from server from the web app

--- src/store/apiService.ts
+++ src/store/apiService.ts
@@ -12,6 +12,7 @@ import User, {
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
+  timeout: 10000,
 });
 // api.defaults.headers.common["Access-Control-Allow-Origin"] = origin;
 // api.defaults.headers.common["Access-Control-Allow-Headers"] =
