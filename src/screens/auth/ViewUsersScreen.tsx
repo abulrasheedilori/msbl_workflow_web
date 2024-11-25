3abc314c7d5290694d508cb99eb23a440a6f14ff
Abdulrasheed Ilori <54009597+abulrasheedilori@users.noreply.github.com>
2024-11-25 12:35:29 +0100

    Update ViewUsersScreen.tsx

--- src/screens/auth/ViewUsersScreen.tsx
+++ src/screens/auth/ViewUsersScreen.tsx
@@ -72,7 +72,7 @@ const ViewUsersScreen = () => {
         You can manage all users, change password, re-assign roles and
         disable/enable users here.
       </p>
-      <section className="w-[80vw] h-[70vh] overflow-auto hide-scrollbar">
+      <section className="w-[80vw] h-[65vh] overflow-auto hide-scrollbar">
         <table className="mx-4 text-sm bg-white border border-gray-300">
           <thead className="sticky top-0">
             <tr className="border border-gray-300 bg-slate-50 text-nowrap text-start whitespace-nowrap">
