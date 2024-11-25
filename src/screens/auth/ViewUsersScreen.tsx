d89ca4437e2e745f727eadb2eecdeab21f3c06b8
Abdulrasheed Ilori <54009597+abulrasheedilori@users.noreply.github.com>
2024-11-25 12:21:34 +0100

    Update ViewUsersScreen.tsx

--- src/screens/auth/ViewUsersScreen.tsx
+++ src/screens/auth/ViewUsersScreen.tsx
@@ -72,21 +72,21 @@ const ViewUsersScreen = () => {
         You can manage all users, change password, re-assign roles and
         disable/enable users here.
       </p>
-      <section className="w-full h-[70vh] overflow-auto hide-scrolbar">
+      <section className="w-[80vw] h-[70vh] overflow-auto hide-scrollbar">
         <table className="mx-4 text-sm bg-white border border-gray-300">
           <thead className="sticky top-0 ">
-            <tr className="border border-gray-300 bg-slate-50 text-wrap">
-              <th className="px-2 py-1 text-start">ID</th>
-              <th className="px-2 py-1 text-start">USERNAME</th>
-              <th className="px-2 py-1 text-start">FULL NAME</th>
-              <th className="px-2 py-1 text-start">EMAIL</th>
+            <tr className="border border-gray-300 bg-slate-50 text-wrap text-start whitespace-nowrap">
+              <th className="px-2 py-1">ID</th>
+              <th className="px-2 py-1">USERNAME</th>
+              <th className="px-2 py-1">FULL NAME</th>
+              <th className="px-2 py-1">EMAIL</th>
               {user?.roles.includes("ROLE_ADMIN") && (
-                <th className="px-2 py-1 text-start">PASSWORD</th>
+                <th className="px-2 py-1">PASSWORD</th>
               )}
               {user?.roles.includes("ROLE_SUPERVISOR") ? (
-                <th className="px-2 py-1 text-start">RE-ASSIGN ROLE</th>
+                <th className="px-2 py-1">RE-ASSIGN ROLE</th>
               ) : (
-                <th className="px-2 py-1 text-start">ROLE</th>
+                <th className="px-2 py-1">ROLE</th>
               )}
               {/* {user?.roles.includes("ROLE_ADMIN") && (
                 <th className="flex-1 px-2 py-1 text-start">ACTION</th>
